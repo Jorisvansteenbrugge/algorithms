0686e1f6fdde698cedb9028c2c2bcf7b12f1b06f
Jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-05-23 19:57:33 +0200

    assignment 1

--- assignment1_joris.py
+++ assignment1_joris.py
@@ -1,6 +1,16 @@
-
+#!/usr/bin/env python
 from __future__ import division
+
+"""
+This script implements the Needleman-Wunsch algorithm
+to align protein seqences
+"""
+
+__author__ = "Joris van Steenbrugge (950416798110)"
+__email__ = " joris.vansteenbrugge@wur.nl"
+
 import argparse 
+
 
 blosum = """
 # http://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
@@ -38,6 +48,11 @@ blosum = """
 """
 
 def parse_arguments():
+    """Return parse arguments object
+
+    Parses the separate sequences, end_gap penalty, regular gap penalty.
+    Only seqA and seqB are required as both gap penalties do have a default value.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-seqA", dest = "seqA", required = True,
         help = "The first protein sequence")
@@ -79,24 +94,42 @@ BLOSUM62_ORDER, BLOSUM62_MATRIX = blosum62()
 def score(res1, res2):
     """Return similarity score from BLOSUM62 matrix for two residues
     
-    res1: string, amino acid
-    res2: string, amino acid
+        Keyword Arguments:
+        res1 -- string, amino acid
+        res2 -- string, amino acid
     """
     lookup1 = BLOSUM62_ORDER[res1]
     lookup2 = BLOSUM62_ORDER[res2]
     return BLOSUM62_MATRIX[lookup1][lookup2]
 
 def score_at_position(i, j, sequences):
+    """Returns the BLOSUM62 score of two residues at a specific position
+
+        Keyword Arguments:
+            i -- integer stringA index
+            j -- integer stringB index
+            sequences -- tuple reversely sorted on length
+                         containing two protein sequences
+    """
     res_a = sequences[0][i - 1]
     res_b = sequences[1][j - 1]
-    # print("{} -> {}".format(res_a, res_b))
+
     scr = score(res_a, res_b)
     return scr
 
 def gap_penalty(n, E):
+    """Return the gap penalty for a certain length
+
+        Keyword Arguments:
+            n -- integer, index of gaps in a row
+            E -- integer, the gap penalty 
+
+    """
     return ((-n) * E)
 
-def create_empty_matrix(sequences, Nones= True):      
+def create_empty_matrix(sequences, Nones= True):     
+    """Return 
+    """
     m = len(sequences[0]) + 1
     n = len(sequences[1]) + 1
     if Nones:
@@ -113,7 +146,7 @@ def matrix_template(sequences, A = 2):
             A -- End gap penalty score
 
         Returns:
-            A matrix implemented as two dimensional list, 
+            A matrix implemented as two dimensional array, 
                 to be queried as matrix[row][col]
     """
     #Create an empty matrix with the corresponding dimensions
@@ -138,7 +171,7 @@ def parent_matrix_template(sequences):
             sequences -- A tuple reversely sorted on length
                          containing two protein sequences 
         Returns:
-            A matrix implemented as two dimensional list,
+            A matrix implemented as two dimensional array,
                 to be queried as matrix[row][col]
     """
     parent_matrix = create_empty_matrix(sequences, False)
@@ -153,43 +186,70 @@ def parent_matrix_template(sequences):
 
 
 def pretty_print(matrix):
+    """Prints a two dimensional array tab separated to the console
+    """
     for line in matrix:
         print "\t".join(map(str, line))
 
 
 
 def fill_matrix(matrix, parent_matrix, sequences, E = 4, A = 2):
+    """Fills the matrix with BLOSUM similarity scores or gaps.
 
+    The matrix is filled according to the Needleman-Wunsch algorithm.
+    each position in the matrix is filled with the best possible alignment      
+    by looking at the previous positions, either left (diagonal left up) 
+    and up of the current position.
+
+        Keyword Arguments:
+            matrix -- two dimensional matrix
+            parent -- two dimensionsl matrix (same dimensions as matrix)
+            E -- int, regular gap penalty
+            A -- int, end gap penalty
+    """
+
+    #iterate all rows
     for i in range(1, len(matrix)):
         left_gap = True
+        #iterate all collumns per row
         for j in range(1, len(matrix[0])):
             up_gap = True
 
+            #left gap indicates if there is an end_gap on the left
+            #if the current reslt would be a gap this would also
+            #use the end gap score instead of the regular one
             if left_gap:
                 left = matrix[i][j-1] + gap_penalty(1, A)
             else:
                 left = matrix[i][j-1] + gap_penalty(1, E)
 
+            #up gap works in a similar fashion as left gap 
             if up_gap:
                 up = matrix[i-1][j] + gap_penalty(1, A)
             else:
                 up = matrix[i-1][j] + gap_penalty(1, E)
             diag = matrix[i-1][j-1] + score_at_position(i, j, sequences)
 
+            #for each position, 3 possible scores are applicable,
+            #the highest score is chosen.
             idx, value = max(enumerate((diag,left,up)), key = lambda x: x[1])
 
-            if idx == 0:
+            if idx == 0: # if diagonal up
                 parent = (i - 1, j - 1)
                 left_gap = False
                 up_gap = False
-            elif idx == 1:
+            elif idx == 1: # if left
                 parent = (i, j - 1)
                 up_gap = False
-            elif idx == 2:
+            elif idx == 2: # if up
                 parent = (i - 1, j)
                 left_gap = False
+
+            #assign the score to the matrix
             matrix[i][j] = value
+            #assign which direction was chosen to the parent matrix
             parent_matrix[i][j] = parent
+
     return matrix, parent_matrix
 
 
@@ -226,6 +286,12 @@ def traceback(matrix, parent_matrix, sequences):
 
         
 def get_identity(seq1, seq2):
+    """Returns the percent identity between two aligned sequences
+        
+        KeywordArguments:
+            seq1 -- string, aligned protein sequence
+            seq2 -- string, aligned protein sequence
+    """
     num_ident = len([1 for i in range(len(seq1)) if seq1[i] == seq2[i]])
     
     return (num_ident * 100) / len(seq1)
@@ -234,34 +300,33 @@ def get_identity(seq1, seq2):
 def align(seq1, seq2, E = 4, A = 2):
     sequences = sorted((seq1, seq2), key = len, reverse = False)
     
-        matrix = matrix_template(sequences, A)
-        parent_matrix = parent_matrix_template(sequences)
-        matrix, parent_matrix = fill_matrix(matrix, parent_matrix, sequences, E, A)
+    matrix = matrix_template(sequences, A)
+    parent_matrix = parent_matrix_template(sequences)
+    matrix, parent_matrix = fill_matrix(matrix, parent_matrix, sequences, E, A)
         
 
-        aligned_seq1, aligned_seq2 = traceback(matrix, parent_matrix, sequences)
-        alignment_indicator = []
-        for i in range(len(aligned_seq2)):
-            if aligned_seq1[i] == aligned_seq2[i]:
-                alignment_indicator.append('|')
-            else:
-                alignment_indicator.append(' ')
+    aligned_seq1, aligned_seq2 = traceback(matrix, parent_matrix, sequences)
+    alignment_indicator = []
+    for i in range(len(aligned_seq2)):
+        if aligned_seq1[i] == aligned_seq2[i]:
+            alignment_indicator.append('|')
+        else:
+            alignment_indicator.append(' ')
+    print(aligned_seq1)
+    print("".join(alignment_indicator))
+    print(aligned_seq2)
+    print("")
+    print("Identity: {:.4f}".format(get_identity(
+                                        aligned_seq1, aligned_seq2)
+                                    )
+         )
 
-        print(aligned_seq1)
-        print("".join(alignment_indicator))
-        print(aligned_seq2)
-        print("")
-        print("Identity: {:.4f}".format(get_identity(
-                                            aligned_seq1, aligned_seq2)
-                                          )
-             )
-
-    if __name__ == "__main__":
-        args = parse_arguments()
-        seq1 = "THISLINE"
-        seq2 = "ISALIGNED"
-        
-        align(args.seqA, args.seqB, E = args.gap_penalty,
-         A = args.end_gap_penalty)
+if __name__ == "__main__":
+    args = parse_arguments()
+    seq1 = "THISLINE"
+    seq2 = "ISALIGNED"
+    
+    align(args.seqA, args.seqB, E = args.gap_penalty,
+     A = args.gap_penalty)
 
 
